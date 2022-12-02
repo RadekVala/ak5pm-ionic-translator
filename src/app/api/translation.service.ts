73679c72c6927fcb34d5d8778dd15b7ab41f5217
Radek Vala <radek.www@gmail.com>
2022-12-02 15:24:22 +0100

    translation service - http get method finalized

--- src/app/api/translation.service.ts
+++ src/app/api/translation.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TranslationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTranslation(input:string){
+    return this.http.get("https://api.mymemory.translated.net/get?q=" + input + "&langpair=en|cs")
+  }
 }
