605226699944ed6b534b5eb00f2548417f89487d
Radek Vala <radek.www@gmail.com>
2022-12-02 14:38:47 +0100

    Initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cz.utb.fai.translatorapp',
   appName: 'TranslatorApp',
   webDir: 'www',
   bundledWebRuntime: false
