bb38a50521aef969a4c1cf12e7b5c420bfa8db8e
Radek Vala <radek.www@gmail.com>
2022-12-02 16:37:15 +0100

    HistoryRecord Model Class fixed

--- src/app/models/history-record.ts
+++ src/app/models/history-record.ts
@@ -1,6 +1,13 @@
 export class HistoryRecord {
+
+  input: string
+  output: string
+
   constructor(
     input:string,
     output: string
-  ){}
+  ){
+    this.input = input
+    this.output = output
+  }
 }
