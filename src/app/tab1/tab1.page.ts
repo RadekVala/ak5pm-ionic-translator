c410ded329ae667feab3d240419066a7121ddf87
Radek Vala <radek.www@gmail.com>
2022-12-02 15:43:50 +0100

    Loading dialog added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslationService } from '../api/translation.service';
 
+import { LoadingController } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -10,14 +12,28 @@ export class Tab1Page {
 
   input:string = ''
   output:string = ''
+  private loading: any
 
-  constructor(private api: TranslationService) {}
+  constructor(
+    private api: TranslationService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   btnTranlateClicked() {
     //this.output = this.input
+    this.showLoading()
     this.api.getTranslation(this.input).subscribe((data:any) => {
       this.output = data.responseData.translatedText
+      this.loading.dismiss();
     })
+  }
+
+  async showLoading() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Searching for translation...',
+    });
+
+    this.loading.present();
   }
 
 }
